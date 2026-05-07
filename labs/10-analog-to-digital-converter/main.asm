dde11582ffde875bc4a25c90f9576a2e87c09fc4
anveretal <anveretal@mail.ru>
2026-05-07 12:16:53 +0700

    update main.asm and circuit for lab 10

--- labs/10-analog-to-digital-converter/main.asm
+++ labs/10-analog-to-digital-converter/main.asm
@@ -1,37 +1,49 @@
 .include "m168Adef.inc"
 
-.cseg
-.org 0x0000
-    jmp reset
+.CSEG
+.ORG 0x0
+    rjmp RESET
 
-reset:
-    ldi r16, high(RAMEND)
-    out SPH, r16
-    ldi r16, low(RAMEND)
-    out SPL, r16
+.ORG INT_VECTORS_SIZE
+RESET:
+    ldi  r16, HIGH(RAMEND)
+    out  SPH, r16
+    ldi  r16, LOW(RAMEND)
+    out  SPL, r16
 
-    ldi r16, 0xFF
-    out DDRD, r16
+    ldi  r16, 0xFF
+    out  DDRD, r16
+    ldi  r16, 0x00
+    out  PORTD, r16
 
-    ldi r16, (1 << REFS0) | (1 << ADLAR)
-    sts ADMUX, r16
+    ldi  r16, 0x03
+    out  DDRB, r16
+    ldi  r16, 0x00
+    out  PORTB, r16
 
+    ; --- Инициализация АЦП ---
+    ; ADMUX: REFS0=1 (опорное = AVCC), ADLAR=0 (правое выравнивание), MUX=0000 (канал ADC0)
+    ldi  r16, (1 << REFS0)
+    sts  ADMUX, r16
+    ; ADCSRA: ADEN=1 (включить АЦП), ADPS2:0=111 (делитель 128)
+    ldi  r16, (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0)
+    sts  ADCSRA, r16
+MAIN_LOOP:
+    lds  r16, ADCSRA
+    ori  r16, (1 << ADSC)
+    sts  ADCSRA, r16
+ADC_WAIT:
+    lds  r16, ADCSRA
+    sbrc r16, ADSC              ; Пропустить следующую команду если ADSC = 0
+    rjmp ADC_WAIT
 
-    ldi r16, (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0)
-    sts ADCSRA, r16
+    lds  r17, ADCL              ; Биты 7-0
+    lds  r18, ADCH              ; Биты 9-8 (в битах 1-0)
 
-main_loop:
-    lds r16, ADCSRA
-    ori r16, (1 << ADSC)
-    sts ADCSRA, r16
+    out  PORTD, r17             ; PORTD
+    out  PORTB, r18             ; PORTB (PB1:PB0)
 
-wait_adc:
-    lds r16, ADCSRA
-    sbrc r16, ADSC
-    rjmp wait_adc
+    rjmp MAIN_LOOP
 
-    lds r18, ADCL
-    lds r17, ADCH
-    out PORTD, r17
-
-    rjmp main_loop
+.DSEG
+.ORG SRAM_START
